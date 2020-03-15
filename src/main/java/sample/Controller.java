147bb229e95ae1303d0f73482e7b28f34be17aae
rflorian2017 <roby_florian@yahoo.com>
2020-03-15 12:24:47 +0200

    Added entity

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -3,6 +3,12 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
+import model.User;
+import repository.UserRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Controller {
     public Button btnLogin;
@@ -16,11 +22,25 @@ public class Controller {
     public TabPane tabPane;
     public Tab tabLogin;
     public Tab tabMain;
+    public TextField txtFieldUsernameRegister;
+    public Button btnRegister;
+    public PasswordField pwdFieldRegister;
+    public PasswordField pwdFieldReEnterPasswordRegister;
+    public MenuItem mnuItemRegister;
+    public Tab tabRegister;
+    private UserRepository userRepository;
 
     public void initialize() {
 //        tabPane.getTabs().remove(tabLogin);
 //        tabPane.getTabs().remove(tabMain);
         tabPane.getTabs().clear();
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TODOFx");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        userRepository = new UserRepository(entityManager);
+
+
     }
 
     public void showMessageInConsole(ActionEvent actionEvent) {
@@ -37,10 +57,17 @@ public class Controller {
             lblInfo.setVisible(true);
             lblInfo.setText("Please fill usernae");
         } else {
-            System.out.println(txtFieldUsername.getText());
-            System.out.println(txtFieldPassword.getText());
-            tabPane.getTabs().remove(tabLogin);
-            tabPane.getTabs().add(tabMain);
+            User user = userRepository.findById(txtFieldUsername.getText()).get();
+            if(user.getPassword().equals(pwdFieldPassword.getText())) {
+                System.out.println(txtFieldUsername.getText());
+                System.out.println(txtFieldPassword.getText());
+                tabPane.getTabs().remove(tabLogin);
+                tabPane.getTabs().add(tabMain);
+            }
+            else {
+                System.out.println("wrong password");
+            }
+
         }
     }
 
@@ -60,5 +87,27 @@ public class Controller {
 
     public void showLoginPane(ActionEvent actionEvent) {
         tabPane.getTabs().add(tabLogin);
+    }
+
+    public void registerUser(ActionEvent actionEvent) {
+        if(!userRepository.findById(txtFieldUsernameRegister.getText()).isPresent()) {
+            if (pwdFieldRegister.getText().equals(pwdFieldReEnterPasswordRegister.getText())) {
+                User user = new User();
+                user.setUsername(txtFieldUsernameRegister.getText());
+                user.setPassword(pwdFieldRegister.getText());
+
+                userRepository.save(user);
+                tabPane.getTabs().add(tabLogin);
+                tabPane.getTabs().remove(tabRegister);
+            }
+        }
+        else {
+            lblInfo.setVisible(true);
+            lblInfo.setText("User already exists");
+        }
+    }
+
+    public void openRegisterTab(ActionEvent actionEvent) {
+        tabPane.getTabs().add(tabRegister);
     }
 }
